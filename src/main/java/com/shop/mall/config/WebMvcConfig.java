c8ae826da7b0a9e27f085f6e294a32fe35443c62
jeonDev <jjhwhdgus0727@gmail.com>
2022-06-08 22:54:14 +0900

    Commit

--- src/main/java/com/shop/mall/config/WebMvcConfig.java
+++ src/main/java/com/shop/mall/config/WebMvcConfig.java
@@ -9,11 +9,13 @@ public class WebMvcConfig implements WebMvcConfigurer{
 
 	/*
 	 * Cors (localhost:8001)
+	 * http://43.200.39.6:8001/
 	 * */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOrigins("http://localhost:8001")
+				.allowedOrigins("http://43.200.39.6:8001/")
 				.maxAge(3600)				// 3600초 동안 preflight 결과를 캐시에 저장
 				.allowCredentials(true)		// Cookie 요청 허용
 				;
